4b42233fbdfc8c40b2af9d07614aa73845823816
yan <a0726h77@gmail.com>
2015-05-07 19:46:09 +0800

    change group_websites model

--- db/orm/group_websites.py
+++ db/orm/group_websites.py
@@ -6,9 +6,13 @@ import db
 class GroupWebsites(db.Base):
     __tablename__ = 'group_websites'
 
-    group_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(255), nullable=False)
-    url = db.Column(db.String(255), primary_key=True)
+    url = db.Column(db.String(255), nullable=False)
+    feed_url = db.Column(db.String(255))
+    feed_cache_date = db.Column(db.DateTime)
+    feed_cache_attempt_date = db.Column(db.DateTime)
 
     # def get_all(self):
     #     # return GroupWebsites.query.all()
--- models/group_websites.py
+++ models/group_websites.py
@@ -11,3 +11,15 @@ class GroupWebsitesModel(object):
 
     def find_all_match_url(self, url):
         return GroupWebsites.query.filter(GroupWebsites.url.like('%%%s%%' % url)).all()
+
+    def save_feed_url(self, id, feed_url):
+        values = {
+            'feed_url': feed_url
+        }
+
+        rows = GroupWebsites.query.filter(GroupWebsites.id == id).update(values)
+
+        db.session.commit()
+
+        if rows:
+            return id
