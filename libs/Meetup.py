20ba3448da4a4a55f5c4ebb99fbf71c0ce26d123
yan <a0726h77@gmail.com>
2017-08-01 09:07:43 +0800

    from meetup/python-api-client change to meetup-api

--- libs/Meetup.py
+++ libs/Meetup.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import datetime
-import os
-import sys
+'''
+2018-08-01
+from meetup/python-api-client change to meetup-api
+'''
 
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../vendor'))
+import time
 
-import meetup_api_client
+import meetup.api
 
 
 class Meetup():
     def __init__(self, key, debug=False):
-        self.meetup = meetup_api_client.Meetup(key)
+        self.meetup = meetup.api.Client(key)
 
     def get_recent_event(self, group_urlname):
-        events = self.meetup.get_events(group_urlname=group_urlname)
+        events = self.meetup.GetEvents(group_urlname=group_urlname)
 
         results = []
         for event in events.results:
             _ = {}
 
-            _['name'] = event.name
-            _['url'] = event.event_url
+            _['name'] = event['name']
+            _['url'] = event['event_url']
 
-            dt = datetime.datetime.strptime(event.time, "%a %b %d %H:%M:00 CST %Y")
-            _['start_datetime'] = dt
+            _['start_datetime'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(event['time'] / 1000))
 
             results.append(_)
 
