87c322dfc59712a658a84e337df9cf31800e2997
yan <a0726h77@gmail.com>
2017-08-01 09:03:57 +0800

    Update event model

--- models/events.py
+++ models/events.py
@@ -6,7 +6,7 @@ from db.orm.events import Events
 
 
 class EventsModel(object):
-    def find_all(self, group_id, url):
+    def find_all(self, url):
         # return Events.query.filter(Events.group_id == group_id, Events.url == url).all()
         return Events.query.filter(Events.group_id == group_id, Events.url.like("%" + url + "%")).all()
 
@@ -19,14 +19,15 @@ class EventsModel(object):
 
         return query.all()
 
-    def add(self, group_id, name, description=None, url=None, start_datetime=None, end_datetime=None):
+    def add(self, group_id=None, name=None, description=None, url=None, start_datetime=None, end_datetime=None, people_count=None):
         data = {
             'group_id': group_id,
             'name': name,
             'description': description,
             'url': url,
             'start_datetime': start_datetime,
-            'end_datetime': end_datetime
+            'end_datetime': end_datetime,
+            'people_count': people_count
         }
 
         event = Events(**data)
